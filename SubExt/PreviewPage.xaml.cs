5be02b1135cfdc312a562a5c2b8b0620cadd325f
Wayne <Wayne@WAYNE-PC>
2016-04-28 01:20:29 +0800

    Compare and merge frames.

--- SubExt/PreviewPage.xaml.cs
+++ SubExt/PreviewPage.xaml.cs
@@ -37,6 +37,7 @@ namespace SubExt
         private SwapChainPanelRenderer m_renderer;
         private MediaReader m_mediaReader;
         private bool m_isRendering;
+        private byte[] m_previousFrame;
         public PreviewPage()
         {
             this.InitializeComponent();
@@ -57,7 +58,9 @@ namespace SubExt
         {
              // Start from beginning
             m_mediaReader.Seek(TimeSpan.FromMilliseconds(sldPreview.Value));
-            for (int i = 0; i < 90; i++)
+
+            StorageFile file = null;
+            for (int i = 0; i < 1200; i++)
             {
                 // Read each frame
                 using (MediaReaderReadResult mediaResult = await m_mediaReader.VideoStream.ReadAsync())
@@ -86,37 +89,38 @@ namespace SubExt
                             using (GrayscaleNegativeEffect grayscaleNegativeEffect = new GrayscaleNegativeEffect(contrastEffect))
                             using (StampEffect stampEffect = new StampEffect(grayscaleNegativeEffect, (int)sliderStampSmoothness.Value, sliderStampThreshold.Value))
                             {
-    
+                                // Render to a bitmap
                                 WriteableBitmap WB = new WriteableBitmap((int)p.SubtitleRect.Width, (int)p.SubtitleRect.Height);
                                 WriteableBitmapRenderer renderer = new WriteableBitmapRenderer(stampEffect, WB);
                                 await renderer.RenderAsync();
 
-                                StorageFile file = await ApplicationData.Current.TemporaryFolder.CreateFileAsync(((int)inputSample.Timestamp.TotalMilliseconds).ToString("D6") + ".jpg", CreationCollisionOption.ReplaceExisting);
-                                using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                                byte[] pixels = PostProcessing(WB);
+                                if (pixels != null)
                                 {
-                                    var propertySet = new Windows.Graphics.Imaging.BitmapPropertySet();
-                                    var qualityValue = new Windows.Graphics.Imaging.BitmapTypedValue(
-                                        1.0, // Maximum quality
-                                        Windows.Foundation.PropertyType.Single
-                                        );
-
-                                    propertySet.Add("ImageQuality", qualityValue);
-                                    BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream, propertySet);                                    
-                                    Stream pixelStream = WB.PixelBuffer.AsStream();
-                                    byte[] pixels = new byte[pixelStream.Length];
-                                    await pixelStream.ReadAsync(pixels, 0, pixels.Length);
-
-                                    if (ConvertToBW(pixels, WB.PixelWidth, WB.PixelHeight))
+                                    file = await ApplicationData.Current.TemporaryFolder.CreateFileAsync(((int)inputSample.Timestamp.TotalMilliseconds).ToString("D8") + ".jpg", CreationCollisionOption.ReplaceExisting);
+                                    using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.ReadWrite))
                                     {
-                                        await file.DeleteAsync(StorageDeleteOption.PermanentDelete);
-                                    }
-                                    else
-                                    {
+                                        var propertySet = new BitmapPropertySet();
+                                        propertySet.Add("ImageQuality", new BitmapTypedValue(1.0, PropertyType.Single));
+                                        BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream, propertySet);
                                         encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Ignore,
                                             (uint)WB.PixelWidth, (uint)WB.PixelHeight,
                                             96.0, 96.0, pixels);
                                         await encoder.FlushAsync();
                                     }
+                                }
+                                else
+                                {
+                                    string filename = file.Name;
+                                    int pos = filename.LastIndexOf("-");
+                                    if (pos > 0)
+                                        filename = filename.Substring(0, pos + 1) + ((int)inputSample.Timestamp.TotalMilliseconds).ToString("D8") + ".jpg";
+                                    else
+                                    {
+                                        string sub = filename.Substring(0, filename.LastIndexOf("."));
+                                        filename = sub + "-" + ((int)inputSample.Timestamp.TotalMilliseconds).ToString("D8") + ".jpg";
+                                    }
+                                    await file.RenameAsync(filename);
                                 }
                             }
                         }
@@ -125,92 +129,6 @@ namespace SubExt
             }
 
             Frame.Navigate(typeof(SubtitlePage), p);
-        }
-        private bool ConvertToBW(byte[] pixels, int width, int height)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    // First turn all pixels to B/W
-                    pixels[(x + y * width) * 4] = (byte)(pixels[(x + y * width) * 4] >= 128 ? 255 : 0);
-                    pixels[(x + y * width) * 4 + 1] = (byte)(pixels[(x + y * width) * 4 + 1] >= 128 ? 255 : 0);
-                    pixels[(x + y * width) * 4 + 2] = (byte)(pixels[(x + y * width) * 4 + 2] >= 128 ? 255 : 0);
-                }
-            }
-
-            for (int y = 0; y < height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    if (y == 0 || y == height - 1 || x == 0 || x == width - 1)
-                    {
-                        FloodFill(pixels, width, height, new Point(x, y), Color.FromArgb(255, 0, 0, 0), Color.FromArgb(255, 255, 255, 255));
-                    }
-                }
-            }
-
-            for (int i = 0; i < pixels.Length; i++)
-            {
-                if (pixels[i] != 255)
-                    return false;
-            }
-            return true;
-        }
-
-        private static bool ColorMatch(Color a, Color b)
-        {
-            
-            return a.Equals(b);
-        }
-
-        private static Color GetPixel(byte[] pixels, int width, double x, double y)
-        {
-            byte a, r, g, b;
-            b = pixels[(int)(x + y * width) * 4];
-            g = pixels[(int)(x + y * width) * 4+1];
-            r = pixels[(int)(x + y * width) * 4+2];
-            a = pixels[(int)(x + y * width) * 4+3];
-            return Color.FromArgb(a, r, g, b);
-        }
-
-        private static void SetPixel(byte[] pixels, int width, double x, double y, Color c)
-        {
-            pixels[(int)(x + y * width) * 4] = c.B;
-            pixels[(int)(x + y * width) * 4 + 1] = c.G;
-            pixels[(int)(x + y * width) * 4 + 2] = c.R;
-            pixels[(int)(x + y * width) * 4 + 3] = c.A;
-        }
-
-        static void FloodFill(byte[] pixels, int width, int height, Point pt, Color targetColor, Color replacementColor)
-        {
-            Queue<Point> q = new Queue<Point>();
-            q.Enqueue(pt);
-            while (q.Count > 0)
-            {
-                Point n = q.Dequeue();
-                if (!ColorMatch(GetPixel(pixels, width, n.X, n.Y), targetColor))
-                    continue;
-                Point w = n, e = new Point(n.X + 1, n.Y);
-                while ((w.X >= 0) && ColorMatch(GetPixel(pixels, width, w.X, w.Y), targetColor))
-                {
-                    SetPixel(pixels, width, w.X, w.Y, replacementColor);
-                    if ((w.Y > 0) && ColorMatch(GetPixel(pixels, width, w.X, w.Y - 1), targetColor))
-                        q.Enqueue(new Point(w.X, w.Y - 1));
-                    if ((w.Y < height - 1) && ColorMatch(GetPixel(pixels, width, w.X, w.Y + 1), targetColor))
-                        q.Enqueue(new Point(w.X, w.Y + 1));
-                    w.X--;
-                }
-                while ((e.X <= width - 1) && ColorMatch(GetPixel(pixels, width, e.X, e.Y), targetColor))
-                {
-                    SetPixel(pixels, width, e.X, e.Y, replacementColor);
-                    if ((e.Y > 0) && ColorMatch(GetPixel(pixels, width, e.X, e.Y - 1), targetColor))
-                        q.Enqueue(new Point(e.X, e.Y - 1));
-                    if ((e.Y < height - 1) && ColorMatch(GetPixel(pixels, width, e.X, e.Y + 1), targetColor))
-                        q.Enqueue(new Point(e.X, e.Y + 1));
-                    e.X++;
-                }
-            }
         }
         private void effects_Changed(object sender, RoutedEventArgs e)
         {
@@ -320,6 +238,112 @@ namespace SubExt
                 }
              }
         }
+        private double CompareFrames(byte[] img1, byte[] img2)
+        {
+            if (img2 == null)
+                return 0;
+
+            byte[] diff = new byte[img1.Length / 4];
+            for (int i = 0; i < img1.Length / 4; i++)
+            {
+                // since the pixels are either black or white, only check the first byte.
+                diff[i] = (byte)(img1[i * 4] == img2[i * 4] ? 1 : 0);
+            }
+
+            double sameCount = 0;
+            foreach (byte item in diff)
+            {
+                if (item == 1)
+                    sameCount++;
+            }
+            return sameCount / (img1.Length / 4);
+        }
+        private void ConvertToBlackWhite(byte[] pixels, int width, int height)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    // First turn all pixels to B/W
+                    pixels[(x + y * width) * 4] = (byte)(pixels[(x + y * width) * 4] >= 128 ? 255 : 0);
+                    pixels[(x + y * width) * 4 + 1] = (byte)(pixels[(x + y * width) * 4 + 1] >= 128 ? 255 : 0);
+                    pixels[(x + y * width) * 4 + 2] = (byte)(pixels[(x + y * width) * 4 + 2] >= 128 ? 255 : 0);
+                }
+            }
+
+            //for (int y = 0; y < height; y++)
+            //{
+            //    for (int x = 0; x < width; x++)
+            //    {
+            //        if (y == 0 || y == height - 1 || x == 0 || x == width - 1)
+            //        {
+            //            FloodFill(pixels, width, height, new Point(x, y), Color.FromArgb(255, 0, 0, 0), Color.FromArgb(255, 255, 255, 255));
+            //        }
+            //    }
+            //}
+
+            //for (int i = 0; i < pixels.Length; i++)
+            //{
+            //    if (pixels[i] != 255)
+            //        return false;
+            //}
+            //return true;
+        }
+
+        private static bool ColorMatch(Color a, Color b)
+        {
+
+            return a.Equals(b);
+        }
+
+        private static Color GetPixel(byte[] pixels, int width, double x, double y)
+        {
+            byte a, r, g, b;
+            b = pixels[(int)(x + y * width) * 4];
+            g = pixels[(int)(x + y * width) * 4 + 1];
+            r = pixels[(int)(x + y * width) * 4 + 2];
+            a = pixels[(int)(x + y * width) * 4 + 3];
+            return Color.FromArgb(a, r, g, b);
+        }
+
+        private static void SetPixel(byte[] pixels, int width, double x, double y, Color c)
+        {
+            pixels[(int)(x + y * width) * 4] = c.B;
+            pixels[(int)(x + y * width) * 4 + 1] = c.G;
+            pixels[(int)(x + y * width) * 4 + 2] = c.R;
+            pixels[(int)(x + y * width) * 4 + 3] = c.A;
+        }
+
+        static void FloodFill(byte[] pixels, int width, int height, Point pt, Color targetColor, Color replacementColor)
+        {
+            Queue<Point> q = new Queue<Point>();
+            q.Enqueue(pt);
+            while (q.Count > 0)
+            {
+                Point n = q.Dequeue();
+                if (!ColorMatch(GetPixel(pixels, width, n.X, n.Y), targetColor))
+                    continue;
+                Point w = n, e = new Point(n.X + 1, n.Y);
+                while ((w.X >= 0) && ColorMatch(GetPixel(pixels, width, w.X, w.Y), targetColor))
+                {
+                    SetPixel(pixels, width, w.X, w.Y, replacementColor);
+                    if ((w.Y > 0) && ColorMatch(GetPixel(pixels, width, w.X, w.Y - 1), targetColor))
+                        q.Enqueue(new Point(w.X, w.Y - 1));
+                    if ((w.Y < height - 1) && ColorMatch(GetPixel(pixels, width, w.X, w.Y + 1), targetColor))
+                        q.Enqueue(new Point(w.X, w.Y + 1));
+                    w.X--;
+                }
+                while ((e.X <= width - 1) && ColorMatch(GetPixel(pixels, width, e.X, e.Y), targetColor))
+                {
+                    SetPixel(pixels, width, e.X, e.Y, replacementColor);
+                    if ((e.Y > 0) && ColorMatch(GetPixel(pixels, width, e.X, e.Y - 1), targetColor))
+                        q.Enqueue(new Point(e.X, e.Y - 1));
+                    if ((e.Y < height - 1) && ColorMatch(GetPixel(pixels, width, e.X, e.Y + 1), targetColor))
+                        q.Enqueue(new Point(e.X, e.Y + 1));
+                    e.X++;
+                }
+            }
+        }
         private async void OpenPreviewVideo()
         {
             if (p.Video != null)
@@ -328,6 +352,38 @@ namespace SubExt
                 sldPreview.Maximum = m_mediaReader.Duration.TotalMilliseconds;
                 sldPreview.Value = 10000;
                 CalculateMediaRect();
+            }
+        }
+        private byte[] PostProcessing(WriteableBitmap source)
+        {
+            byte[] sourcePixels = source.PixelBuffer.ToArray();
+            // Change to black and white first
+            ConvertToBlackWhite(sourcePixels, source.PixelWidth, source.PixelHeight);
+
+            // Floodfile to clear the edges
+            for (int y = 0; y < source.PixelHeight; y++)
+            {
+                for (int x = 0; x < source.PixelWidth; x++)
+                {
+                    if (y == 0 || y == source.PixelHeight - 1 || x == 0 || x == source.PixelWidth - 1)
+                    {
+                        FloodFill(sourcePixels, source.PixelWidth, source.PixelHeight, new Point(x, y), Color.FromArgb(255, 0, 0, 0), Color.FromArgb(255, 255, 255, 255));
+                    }
+                }
+            }
+
+            // Resize 
+
+
+            // Compare with previous frame
+            if (CompareFrames(sourcePixels, m_previousFrame) > 0.98)
+            {
+                return null;
+            }
+            else
+            {
+                m_previousFrame = sourcePixels;
+                return sourcePixels;
             }
         }
         private async void SeekVideo(TimeSpan position)
